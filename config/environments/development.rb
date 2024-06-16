8bef83d5e30e14a7ab423b187c4b24758b6408c2
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-06-16 17:24:15 +0900

    Add: ngrok用設定を追加, docker関連ファイルを修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "96c2-103-5-140-163.ngrok-free.app"
+  Rails.application.routes.default_url_options[:host] = "96c2-103-5-140-163.ngrok-free.app"
 end
