4b28d06ccaceddb076e764fe2a565612a965e40e
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-06-18 15:43:09 +0900

    Add: 管理画面およびオーダー用QR新規作成機能を追加

--- app/models/order_unit.rb
+++ app/models/order_unit.rb
@@ -1,4 +1,19 @@
 class OrderUnit < ApplicationRecord
   has_one :order, dependent: :restrict_with_error
   validates :code, presence: true, uniqueness: true
+
+  def qrcode
+    qrcode = RQRCode::QRCode.new(Rails.application.routes.url_helpers.order_unit_url(self))
+    svg = qrcode.as_svg(
+      color: "000",
+      shape_rendering: "crispEdges",
+      module_size: 4,
+      standalone: true,
+      use_path: true
+    ).html_safe
+  end
+
+  def to_param
+    code
+  end
 end
